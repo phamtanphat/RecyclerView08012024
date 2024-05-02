178f5670181a8aa3f13fe9a161cdf3ca76c5e801
phamtanphat <phatdroid94@gmail.com>
2024-05-02 18:53:52 +0700

    data

--- app/src/main/java/com/example/recyclerview08012024/Cinema.kt
+++ app/src/main/java/com/example/recyclerview08012024/Cinema.kt
@@ -12,7 +12,12 @@ class Cinema (
 
         fun getDataMock(): List<Cinema> {
             return mutableListOf(
-                Cinema(R.drawable.galaxy_nguyen_du, "Galaxy Nguyễn", "116 Nguyễn Du, Quận 1, Tp.HCM", "19002224", 461)
+                Cinema(R.drawable.galaxy_nguyen_du, "Galaxy Nguyễn Du", "116 Nguyễn Du, Quận 1, Tp.HCM", "19002224", 461),
+                Cinema(R.drawable.galaxy_tan_binh, "Galaxy Tân Bình", "246 Nguyễn Hồng Đào, Quận TB, Tp.HCM", "19002224", 5800),
+                Cinema(R.drawable.galaxy_pham_van_tri, "Galaxy Phạm Văn Chí", "634Bis Phạm Văn Chí, Phường 8, Quận 6, Tp.HCM", "19002224", 7100),
+                Cinema(R.drawable.galaxy_quang_trung, "Galaxy Quang Trung", "Lầu 3, TTTM CoopMart Foodcosa số 304A, Quang Trung, P.11, Q. Gò Vấp, Tp.HCM", "19002224", 7200),
+                Cinema(R.drawable.galaxy_kinh_duong_vuong, "Galaxy Kinh Dương Vương", "718bis Đ. Kinh Dương Vương, Phường 13, Quận 6, Tp.HCM", "19002224", 7500),
+                Cinema(R.drawable.galaxy_huynh_tan_phat, "Galaxy Cinema Huỳnh Tấn Phát", "1362 Huỳnh Tấn Phát, Phú Mỹ, Quận 7, Tp.HCM", "19002224", 8700)
             )
         }
     }
